80c7fa9ba9ec2a16ac2b97f9fa36904545a31499
Yousef Sami <yousef28s10@gmail.com>
2023-05-04 23:29:11 +0300

    v1

---
+++ app/src/main/java/com/example/arabworld/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.arabworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn1;    private Button btn2;
+    private Button btn3;    private Button btn4;
+    private Button btn5;    private Button btn6;
+    private Button btn7;    private Button btn8;
+    private Button btn9;    private Button btn10;
+    private Button btn11;   private Button btn12;
+    private Button btn13;   private Button btn14;
+    private Button btn15;   private Button btn16;
+    private Button btn17;   private Button btn18;
+    private Button btn19;   private Button btn20;
+    private Button btn21;   private Button btn22;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
