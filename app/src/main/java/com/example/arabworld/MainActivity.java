8adef3542892bc3bf7f5a349e4f587b0939fb365
Yousef Sami <yousef28s10@gmail.com>
2023-05-05 01:56:10 +0300

    v1

--- app/src/main/java/com/example/arabworld/MainActivity.java
+++ app/src/main/java/com/example/arabworld/MainActivity.java
@@ -23,5 +23,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btn10 = (Button) findViewById(R.id.btn10);
+        btn11 = (Button) findViewById(R.id.btn11);
+        btn12 = (Button) findViewById(R.id.btn12);
+        btn13 = (Button) findViewById(R.id.btn13);
+        btn14 = (Button) findViewById(R.id.btn14);
+        btn15 = (Button) findViewById(R.id.btn15);
+        btn16 = (Button) findViewById(R.id.btn16);
+        btn17 = (Button) findViewById(R.id.btn17);
+        btn18 = (Button) findViewById(R.id.btn18);
+        btn19= (Button) findViewById(R.id.btn19);
+        btn20 = (Button) findViewById(R.id.btn20);
+        btn21 = (Button) findViewById(R.id.btn21);
+        btn22 = (Button) findViewById(R.id.btn22);
+
+
+        
+
+
     }
 }
