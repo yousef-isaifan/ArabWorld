d703fa3bb8a4d27cc4fcfcbcf6a68288c7571596
Yousef Sami <yousef28s10@gmail.com>
2023-05-05 21:58:38 +0300

    v1

--- app/src/main/java/com/example/arabworld/model/CountryDA.java
+++ app/src/main/java/com/example/arabworld/model/CountryDA.java
@@ -1,4 +1,30 @@
 package com.example.arabworld.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CountryDA {
+    private ArrayList<Country> countries;
+
+    public CountryDA() {
+
+    }
+
+    private void readFile() {
+        File file = new File("text.txt");
+        System.out.println(file.exists());
+        try {
+            Scanner scan = new Scanner(file);
+            String read = "";
+            while (scan.hasNext()) {
+                read+=scan.next();
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
